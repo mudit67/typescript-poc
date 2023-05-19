e82bbe42aa0308ebc20143b8e3a74d18044c51ac
mudit67 <bhardwajmudit006@gmail.com>
2023-05-19 14:27:40 +0530

    lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,30 @@
-var character = "mario";
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// Commented code is invalid typescript
+// Arrays
+var names = ["mudit", "unnat", "daksh"];
+names.push("kushagra");
+// names.push(3)
+// names[0] = 7;
+var numbers = [1, 2, 34, 5];
+numbers.push(77);
+// numbers.push("modi");
+var mixed = ["luigi", 24, "shangi-chi", 7, 8, true];
+mixed.push("ru");
+mixed.push(70);
+mixed[0] = 7;
+mixed[1] = 7;
+// Objects
+var person7 = {
+    name: "modi",
+    level: "beginner",
+    age: 18,
 };
-console.log(circ(7));
+person7.age = 40;
+person7.name = "muddy";
+// person7.age = "24";
+// person7.skills = ["coding","running"];
+person7 = {
+    name: "unnat",
+    age: 40,
+    level: "advanced",
+    // skills: ["coding", "gaming"],
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,7 +1,43 @@
-let character = "mario";
+// Commented code is invalid typescript
 
-const circ = (diameter:number) => {
-  return diameter * Math.PI;
+// Arrays
+
+let names = ["mudit", "unnat", "daksh"];
+
+names.push("kushagra");
+
+// names.push(3)
+
+// names[0] = 7;
+
+let numbers = [1, 2, 34, 5];
+
+numbers.push(77);
+// numbers.push("modi");
+
+let mixed = ["luigi", 24, "shangi-chi", 7, 8, true];
+
+mixed.push("ru");
+mixed.push(70);
+mixed[0] = 7;
+mixed[1] = 7;
+
+// Objects
+
+let person7 = {
+  name: "modi",
+  level: "beginner",
+  age: 18,
 };
 
-console.log(circ(7))
+person7.age = 40;
+person7.name = "muddy";
+// person7.age = "24";
+// person7.skills = ["coding","running"];
+
+person7 = {
+  name: "unnat",
+  age: 40,
+  level: "advanced",
+  // skills: ["coding", "gaming"],
+};
