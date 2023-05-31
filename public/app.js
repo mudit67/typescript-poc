7cd78e2f42ecd6c07ac8e497ac22704c800d378e
mudit67 <bhardwajmudit006@gmail.com>
2023-05-31 13:58:59 +0530

    lesson 19 enums

--- public/app.js
+++ public/app.js
@@ -1,24 +1,6 @@
 import Invoice from "./classes/invoice.js";
 import ListTemplate from "./classes/listTemplate.js";
 import { Payment } from "./classes/payments.js";
-// let docOne: hasFormatter;
-// let docTwo: hasFormatter;
-// docOne = new Invoice("krish", "website", 1000);
-// docTwo = new Payment("darm", "lorem", 1234);
-let docs = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const inv1 = new Invoice("mario", "work on the new blog", 1000);
-// const inv2 = new Invoice("modi", "coding", 100000);
-// let invoices: Invoice[] = [];
-//
-// invoices.push(inv1);
-// invoices.push(inv2);
-//
-// invoices.forEach((inv) => {
-//   console.log(inv.format());
-// });
 const form = document.querySelector("form.new-item-form");
 // input fields
 const type = document.querySelector("#type");
@@ -39,23 +21,22 @@ form.addEventListener("submit", (e) => {
     list.render(doc, type.value, "end");
     console.log(doc);
 });
-// Generics
-const addUID = (obj) => {
-    let Uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { Uid });
+// Enums
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["Book"] = 0] = "Book";
+    ResourceType[ResourceType["Person"] = 1] = "Person";
+    ResourceType[ResourceType["Flash_Drive"] = 2] = "Flash_Drive";
+    ResourceType[ResourceType["Movie"] = 3] = "Movie";
+})(ResourceType || (ResourceType = {}));
+const docOne = {
+    uid: 12,
+    resourceType: ResourceType.Book,
+    data: { title: "the power of the sun" },
 };
-const docOne = addUID({ name: "modi", age: 47 });
-// const docTwo = addUID({ skills: "codings", desig: "manager" }); //Invalid as doesn't have the name attribute/ key-value pair
-console.log(docOne);
-const docThree = {
-    uid: 1234,
-    resourceName: "me",
-    data: "qwerty",
-    // data: {name:"qwerty"},
+const docTwo = {
+    uid: 345,
+    resourceType: ResourceType.Flash_Drive,
+    data: { name: "in the palm of my hand" },
 };
-const docFour = {
-    uid: 123,
-    resourceName: "asf",
-    data: ["sup"],
-};
-console.log(docThree, docFour);
+console.log(docOne, docTwo);
--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,6 @@ import Invoice from "./classes/invoice.js";
 import ListTemplate from "./classes/listTemplate.js";
 import { Payment } from "./classes/payments.js";
 import { hasFormatter } from "./interfaces/hasFormatter.js";
-
-// let docOne: hasFormatter;
-// let docTwo: hasFormatter;
-
-// docOne = new Invoice("krish", "website", 1000);
-// docTwo = new Payment("darm", "lorem", 1234);
-
-let docs: hasFormatter[] = [];
-
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const inv1 = new Invoice("mario", "work on the new blog", 1000);
-// const inv2 = new Invoice("modi", "coding", 100000);
-// let invoices: Invoice[] = [];
-//
-// invoices.push(inv1);
-// invoices.push(inv2);
-//
-// invoices.forEach((inv) => {
-//   console.log(inv.format());
-// });
-
 const form = document.querySelector("form.new-item-form") as HTMLFormElement;
 
 // input fields
@@ -51,40 +26,30 @@ form.addEventListener("submit", (e) => {
   console.log(doc);
 });
 
-// Generics
+// Enums
 
-const addUID = <T extends { name: string }>(obj: T) => {
-  let Uid = Math.floor(Math.random() * 100);
-  return { ...obj, Uid };
-};
+enum ResourceType {
+  Book,
+  Person,
+  Flash_Drive,
+  Movie,
+}
 
-const docOne = addUID({ name: "modi", age: 47 });
-
-// const docTwo = addUID({ skills: "codings", desig: "manager" }); //Invalid as doesn't have the name attribute/ key-value pair
-
-console.log(docOne);
-
-// console.log(docTwo.skills);
-
-// with Interfaces
-
-interface resource<T> {
+interface Resource<T> {
   uid: number;
-  resourceName: string;
+  resourceType: ResourceType;
   data: T;
 }
 
-const docThree: resource<string> = {
-  uid: 1234,
-  resourceName: "me",
-  data: "qwerty",
-  // data: {name:"qwerty"},
+const docOne: Resource<object> = {
+  uid: 12,
+  resourceType: ResourceType.Book,
+  data: { title: "the power of the sun" },
+};
+const docTwo: Resource<object> = {
+  uid: 345,
+  resourceType: ResourceType.Flash_Drive,
+  data: { name: "in the palm of my hand" },
 };
 
-const docFour: resource<string[]> = {
-  uid: 123,
-  resourceName: "asf",
-  data: ["sup"],
-};
-
-console.log(docThree, docFour);
+console.log(docOne, docTwo);
